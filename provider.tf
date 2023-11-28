19adebcc8c9b35ce609ce48c94d142c6f692e31f
divyasree16 <kdivyasree50@gmail.com>
2023-11-28 21:24:49 +0530

    s3 bucket creation

--- provider.tf
+++ provider.tf
@@ -10,4 +10,6 @@ terraform {
 provider "aws" {
   # Configuration options
   region = "us-east-1"
+  access_key = "AKIAVCUZT5AMTXCPF3C4"
+  secret_key = "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"
 }
